970b3fe703250bbe11cdeb46a90f6d93d52ca466
Ekaterina B Bonova <e.bobova2016@gmail.com>
2024-01-18 16:07:19 +0300

    workaet

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,5 @@
 import readlineSync from 'readline-sync';
 
-const name = readlineSync.question('Your answer: ');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+return name
