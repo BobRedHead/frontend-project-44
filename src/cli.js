13b4a19e13f89a149a9e59f15839fba52689e6a2
Ekaterina B Bonova <e.bobova2016@gmail.com>
2024-01-22 22:24:54 +0300

    workaet

--- src/cli.js
+++ src/cli.js
@@ -15,6 +15,32 @@ const greet = () => {
   return getName();
 };
 
+const printWrongAnswMsg = (userName, userAnswer, correctAnswer) => {
+  console.log(`'${userAnswer} is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  console.log(`Let's try again, ${userName}!`);
+};
+
+const printCorrectMsg = () => {
+  console.log('Correct!');
+};
+
+const printEndGameMsg = (userName) => {
+  console.log(`Congratulations, ${userName}!`);
+}
+
+const printQuestion = (value) => {
+  console.log(`Question: ${value}`);
+};
+
+const getUserAnswer = () => readlineSync.question('Your answer: ');
+
+const printGameRules = (rules) => {
+  for (let i = 0; i < rules.length; i += 1) {
+    console.log(rules[i]);
+  }
+};
+
+
 export {
-  greet 
+  greet, printCorrectMsg, printEndGameMsg, getUserAnswer, printGameRules, printQuestion, printWrongAnswMsg
 }
---
+++ src/indexTasks.js
@@ -0,0 +1,18 @@
+const isEven = (n) = (n % 2 === 0);
+
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
+
+const interpolate = (value) => String(value).toLowerCase().trim();
+
+const castYesNo = (val) => {
+  const values = ['no', 'yes'];
+  const i = Number(val);
+  if (Number.isNaN(i)) {
+    return values[0];
+  }
+  return values[i % values.length];
+};
+
+export {
+  castYesNo, isEven, getRandomNumber, interpolate,
+};
